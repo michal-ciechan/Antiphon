15b9d7251fc5f11f301c8e00c61b314dd2b67c87
Mike Ciechan <lndcobra@hotmail.com>
2026-09-26 09:51:14 +0100

    test(CARD-0726): add the missing usings on the alarm delivery tests
    CP-5 still failed to compile: the notifier test needs AppDbContext and the delivery tests need TranscriptKinds.

--- tests/Antiphon.Tests/Application/RunnerAlarmNotifierTests.cs
+++ tests/Antiphon.Tests/Application/RunnerAlarmNotifierTests.cs
@@ -1,5 +1,6 @@
 using Antiphon.Server.Application.Services;
 using Antiphon.Server.Domain.Entities;
+using Antiphon.Server.Infrastructure.Data;
 using Antiphon.Server.Domain.Enums;
 using Antiphon.Tests.TestHelpers;
 using Microsoft.EntityFrameworkCore;
